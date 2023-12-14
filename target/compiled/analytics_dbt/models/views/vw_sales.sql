a9f657aa9fb5ac695a6c4e111d536b9b56baa53a
ssantosfer <santosilvafernanda123@gmail.com>
2023-12-14 20:33:02 -0300

    creation new model with name alteration and format

---
+++ target/compiled/analytics_dbt/models/views/vw_sales.sql
@@ -0,0 +1,24 @@
+with source_sales AS (
+    SELECT
+        *
+    FROM sales
+),
+renamed AS (
+    SELECT
+        SALESID AS ID_VENDA,
+        LISTID AS ID_LISTA,
+        SELLERID AS ID_VENDEDOR,
+        BUYERID AS ID_COMPRADOR,
+        EVENTID AS ID_EVENTO,
+        DATEID AS ID_DATE,
+        QTYSOLD AS QUANTIDADE_VENDIDA,
+        PRICEPAID AS VALOR_PAGO,
+        COMMISSION AS COMISSÃO,
+        TO_CHAR(SALETIME, 'dd/mm/yyyy HH24:MI:SS') AS DATA_HORA_VENDA,
+        CAST(SALETIME AS DATE) AS DATA_VENDA,
+        TO_CHAR(SALETIME, 'HH24:MI:SS') AS HORA_VENDA
+    FROM source_sales
+)
+SELECT 
+    *
+FROM renamed
